2c0ed2c52fc6a19f301c09967792e3eb3fd65993
Sumit Jha <developersumit99@gmail.com>
2025-02-05 15:28:28 +0545

    Added Plugin Header Requirement

--- nepweb-wc-addon.php
+++ nepweb-wc-addon.php
@@ -1,4 +1,7 @@
 <?php
 /*
-
-*/
+ * Plugin Name:       Nepweb Wc Addon
+ * Plugin URI:        https://nepwebtech.com/plugins/nepweb-wc-addon/
+ * Description:       Handle the addons for Woocommerce based Website.
+ * Version:           1.0.0
+ */
